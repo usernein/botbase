c63c5c0de4381e5a0119f4247d62d9a8379d6c7f
Cezar H <29507335+usernein@users.noreply.github.com>
2020-04-03 18:35:39 -0300

    Fixes

--- login.py
+++ login.py
@@ -13,10 +13,13 @@ def raise_ex(e):
     
 print('Creating config.ini...')
 config = configparser.ConfigParser()
-config['pyrogram'] = {}    
-config['pyrogram']['api_id'] = (argv[1] if 1 < len(argv) else input('Input your api_id: ')) or raise_ex(ValueError('Invalid api_id'))
-config['pyrogram']['api_hash'] = (argv[2] if 2 < len(argv) else input('Input your api_hash: ')) or raise_ex(ValueError('Invalid api_hash'))
-config['pyrogram']['bot_token'] = (argv[3] if 3 < len(argv) else input('Input the bot token: ')) or raise_ex(ValueError('Invalid bot token'))
+if os.path.exists('config.ini'):
+    config.read('config.ini')
+    print("Loaded existing config.ini. If you don't specify a value, the existing one will be used.")
+config.setdefault('pyrogram', {})
+config['pyrogram']['api_id'] = input('Input your api_id: ') or config['pyrogram'].get('api_id') or raise_ex(ValueError('Invalid api_id'))
+config['pyrogram']['api_hash'] = input('Input your api_hash: ') or config['pyrogram'].get('api_hash') or raise_ex(ValueError('Invalid api_hash'))
+config['pyrogram']['bot_token'] = input('Input the bot token: ') or config['pyrogram'].get('bot_token') or raise_ex(ValueError('Invalid bot token'))
 
 with open('config.ini','w') as fp:
     config.write(fp)
@@ -25,15 +28,14 @@ async def init():
     print('Logging in and creating .session file...')
     client = Client('bot', plugins={'enabled':False})
     await client.start()
-    print("\nYour PYROGRAM_SESSION (SENSITIVE DATA, DO NOT SHARE):")
-    cprint(client.export_session_string()+"\n", 'green')
     
     print("\nYour PYROGRAM_CONFIG (SENSITIVE DATA, DO NOT SHARE):")
     cprint(base64.b64encode(json.dumps({k:v for section in config.sections() for k,v in config.items(section)}, separators=(',', ':')).encode()).decode()+"\n", 'blue')
-    
+
+    print("\nYour PYROGRAM_SESSION (SENSITIVE DATA, DO NOT SHARE):")
+    cprint(client.export_session_string()+"\n", 'green')
     await client.stop()
 
 loop = asyncio.get_event_loop()
-if not os.path.exists('bot.session'):
-    loop.run_until_complete(init())
+loop.run_until_complete(init())
 print("Done.")
